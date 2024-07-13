2264ea496e1216ced3df9692598acd062426b14d
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-13 10:47:39 +0700

    get all users

---
+++ backend/controllers/user/userControllers.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const prisma = require('../../prisma/client');
+
+//get all users
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+            orderBy: {
+                id: "desc",
+            }
+        });
+
+        res.status(200).send({
+            success: true,
+            messages: "Get all users successfully",
+            data: users,
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+module.exports = { getAllUsers }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const verifyToken = require("../middlewares/auth");
+
+//auth
 const RegisterControllers = require('../controllers/auth/RegisterControllers');
 const LoginControllers = require("../controllers/auth/LoginControllers");
 
+//user
+const userControllers = require("../controllers/user/userControllers");
+
 const { validateRegister, validateLogin } = require('../utils/validator/auth');
 
+//auth
 router.post('/register', validateRegister, RegisterControllers.register);
 router.post('/login', validateLogin, LoginControllers.login);
+
+//users
+router.get('/admin/users', verifyToken, userControllers.getAllUsers);
 
 module.exports = router
