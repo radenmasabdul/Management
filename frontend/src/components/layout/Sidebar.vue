891404ca6c653e7016b84efe11393f6450cd9f62
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-28 00:06:51 +0700

    add menu on sidebar

--- frontend/src/components/layout/Sidebar.vue
+++ frontend/src/components/layout/Sidebar.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+const textClass = "font-JakartaSans text-black font-bold text-sm";
+const iconClass = "text-blue-500 flex-shrink-0 w-5 h-5";
+</script>
 
 <template>
   <div class="lg:drawer-open bg-base-200 z-50">
@@ -13,38 +16,57 @@
       <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
         <li>
           <router-link to="/home">
-            <svg
-              class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 21"
-            >
-              <path
-                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
-              />
-              <path
-                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
-              />
-            </svg>
-            <span class="ms-3">Dashboard</span>
+            <font-awesome-icon :icon="['fas', 'chart-pie']" :class="iconClass" />
+            <span :class="textClass">Dashboard</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <font-awesome-icon :icon="['fas', 'book']" :class="iconClass" />
+            <span :class="textClass">Reports</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <font-awesome-icon :icon="['fas', 'tag']" :class="iconClass" />
+            <span :class="textClass">Submission of Collateral</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <font-awesome-icon :icon="['fas', 'folder']" :class="iconClass" />
+            <span :class="textClass">Approval</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <font-awesome-icon :icon="['fas', 'file-lines']" :class="iconClass" />
+            <span :class="textClass">Warranty Data</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <font-awesome-icon :icon="['fas', 'folder-open']" :class="iconClass" />
+            <span :class="textClass">Surety Bond Data</span>
           </router-link>
         </li>
 
         <li>
           <router-link to="/users">
-            <svg
-              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 18"
-            >
-              <path
-                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"
-              />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+            <font-awesome-icon :icon="['fas', 'user']" :class="iconClass" />
+            <span :class="textClass">Users</span>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="/#">
+            <font-awesome-icon :icon="['fas', 'gear']" :class="iconClass" />
+            <span :class="textClass">Settings</span>
           </router-link>
         </li>
       </ul>
