d2c5b29650972dc649a305ecb9de052900b014a8
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-29 10:03:58 +0700

    installation chart js & implementasi charts

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Layout from "../layouts/Layout.vue";
+import ChartsReports from "../components/dashboard/ChartsReports.vue";
+import ChartsUsers from "../components/dashboard/ChartsUsers.vue";
 </script>
 
 <template>
   <Layout>
-    <div>
-      <h1>INI ISI KONTEN</h1>
+    <div class="py-2">
+      <div class="flex flex-wrap justify-center items-center gap-4">
+        <ChartsReports />
+        <ChartsUsers />
+      </div>
     </div>
   </Layout>
 </template>
